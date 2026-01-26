024a63275d902bf849fa195dfe9f5fc988532a6c
synnelefdal <synnelef@stud.ntnu.no>
2026-01-26 16:05:08 +0100

    Laga kode for DiD wiii + endringer

--- DiD.py
+++ DiD.py
@@ -66,6 +66,6 @@ def Difference_in_Difference(dataset, price_area):
     return DiD
 
 
-print('Prosent endring i NO1:',Difference_in_Difference(data,'NO1'), '\n')
+print('Prosent endring i NO1:',Difference_in_Difference(data,'NO1'), '\n', '\n')
 
-print('Prosent endring i NO5:',Difference_in_Difference(data,'NO5'), '\n')
+print('Prosent endring i NO5:',Difference_in_Difference(data,'NO5'), '\n', '\n')
