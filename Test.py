f1c892eff220d199ed983b41da0d1fb23e7b36e0
synnelefdal <synnelef@stud.ntnu.no>
2026-01-26 14:22:18 +0100

    endringer i test:P

--- Test.py
+++ Test.py
@@ -1,1 +1,16 @@
 print("thisisatest:P")
+
+
+'''
+Difference in difference 
+Først: 
+filtrere datasettet ut ifra ka me vil se på, starte med 3 mnd for eksempel
+regne ut forbruk for disse 3 mnd 
+regne ut forbruk for de forrige 3 mnd 
+dele på hverandre for å finne endring i forbruk 
+printe dette 
+
+og så gjøre det samme for norgespris datasett (kan ikkje endå) og sammenligne  
+
+
+'''
