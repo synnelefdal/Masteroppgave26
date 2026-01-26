b51e93580dcb8bfff8aa74c30922bf4849791b81
KristineMoen <kjmoen@stud.ntnu.no>
2026-01-26 13:00:19 +0100

    Prøver

---
+++ DiD.py
@@ -0,0 +1,10 @@
+# Difference in Difference
+
+import numpy as np
+import pandas as pd
+import math
+import matplotlib.pyplot as plt
+import csv
+import row
+
+data = pd.read_csv('filtered_NO1_NO5_households_with_date_hour.csv')
