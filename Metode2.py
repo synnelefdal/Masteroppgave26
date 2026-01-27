268bb46c0f026552716edff52b4025751e4b381a
KristineMoen <kjmoen@stud.ntnu.no>
2026-01-27 11:21:00 +0100

    Prøver

--- Metode2.py
+++ Metode2.py
@@ -118,7 +118,7 @@ def metode2(data, price_area, Temp):
     print(df)
 
     formula = (
-        'Q("kWh/Metering_point") ~ '
+        'np.log(Q("kWh/Metering_point")) ~ '
         'C(Group, Treatment(reference="Before_ref")) + '
         'Temp24 + I(Temp24**2) + I(Temp24**3) + '
         'Temp72 + C(Hour, Treatment(reference="1")) + '
