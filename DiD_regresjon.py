992af5da7f9291f22fe511cb12f5ad7637847a71
KristineMoen <kjmoen@stud.ntnu.no>
2026-02-13 18:02:25 +0100

    Endringer

--- DiD_regresjon.py
+++ DiD_regresjon.py
@@ -7,24 +7,41 @@ import row
 import statsmodels.api as sm
 import patsy
 
-data_mNP = pd.read_csv('Ikke_i_bruk/Forbruk_NO1_NO5.csv')
-data_uNP = pd.read_csv('Ikke_i_bruk/Forbruk_NO1_NO5.csv')
+data_mNP = pd.read_csv('All_Demand_Data/NO1_mNP.csv', sep= ';')
+data_uNP = pd.read_csv('All_Demand_Data/NO1_uNP.csv', sep= ';')
 
 Temp_Bergen = pd.read_csv('Temp_Bergen.csv')
 Temp_Oslo = pd.read_csv('Temp_Oslo.csv')
 
 
-def DifferenceinDifference(data, price_area, Temp):
+def DifferenceinDifference(data_mNP, data_uNP, price_area, Temp):
     # ------------------- Filterer for dato ---------- #
     start_date_before = '2024-10-01'
     end_date_before = '2025-01-31'
 
     start_date_after = '2025-10-01'
-    end_date_after = '2025-01-31'
+    end_date_after = '2026-01-31'
+
+    # ----------- Endring til Date og Hour ------------ #
+    data_mNP['start_time_utc'] = pd.to_datetime(data_mNP['start_time_utc'],
+                                                format = '%Y-%m-%d %H:%M:%S',
+                                                errors = 'coerce',
+                                                utc = True)
+
+    data_mNP['Date'] = data_mNP['start_time_utc'].dt.date
+    data_mNP['Hour'] = data_mNP['start_time_utc'].dt.hour.astype(int)
+
+    data_uNP['start_time_utc'] = pd.to_datetime(data_uNP['start_time_utc'],
+                                                format='%Y-%m-%d %H:%M:%S',
+                                                errors='coerce',
+                                                utc=True)
+
+    data_uNP['Date'] = data_uNP['start_time_utc'].dt.date
+    data_uNP['Hour'] = data_uNP['start_time_utc'].dt.hour.astype(int)
 
     # ----------------- Demand; NP-Gruppen ------------------ #
 
-    data_demand_NP = data_mNP[data['price_area'] == price_area].copy()
+    data_demand_NP = data_mNP[data_mNP['price_area'] == price_area].copy()
     data_demand_NP['Date'] = pd.to_datetime(data_demand_NP['Date'])
     data_demand_NP['Hour'] = data_demand_NP['Hour'].astype(int)
 
@@ -33,24 +50,23 @@ def DifferenceinDifference(data, price_area, Temp):
 
     data_demand_NP_filtered_after = data_demand_NP[(data_demand_NP['Date'] >= start_date_after) &
                                         (data_demand_NP['Date'] <= end_date_after)].copy()
-    # print(data_demand_filtered1)
-    # print(data_demand_filtered2)
+    #print(data_demand_NP_filtered_before)
+    #print(data_demand_NP_filtered_after)
 
-    for index, rad in data_demand_NP_filtered_before.iterrows():
-        data_demand_NP_filtered_before.loc[index, 'kWh/Metering_point'] = rad['quantity_kwh'] / rad['metering_point_count']
+    data_demand_NP_filtered_before['kWh/Metering_point'] = data_demand_NP_filtered_before['consumption_kwh'] / data_demand_NP_filtered_before['metering_point_count']
+    #print(data_demand_NP_filtered_before.head(3))
 
-    for index, rad in data_demand_NP_filtered_after.iterrows():
-        data_demand_NP_filtered_after.loc[index, 'kWh/Metering_point'] = rad['quantity_kwh'] / rad['metering_point_count']
-    # print(data_demand_filtered2.head(3))
+    data_demand_NP_filtered_after['kWh/Metering_point'] = data_demand_NP_filtered_after['consumption_kwh'] / data_demand_NP_filtered_after['metering_point_count']
+    #print(data_demand_NP_filtered_after.head(3))
 
-    total_demand_hour_NP_before = data_demand_NP_filtered_before.groupby(['Date', 'Hour'])['kWh/Metering_point'].sum().reset_index()
-    total_demand_hour_NP_after = data_demand_NP_filtered_after.groupby(['Date', 'Hour'])['kWh/Metering_point'].sum().reset_index()
-    # print(total_hour1)
-    # print(total_hour2)
+    total_demand_hour_NP_before = data_demand_NP_filtered_before.groupby(['Date', 'Hour', 'group_definition'])['kWh/Metering_point'].sum().reset_index()
+    total_demand_hour_NP_after = data_demand_NP_filtered_after.groupby(['Date', 'Hour', 'group_definition'])['kWh/Metering_point'].sum().reset_index()
+    #print(total_demand_hour_NP_before)
+    #print(total_demand_hour_NP_after)
 
     # ----------------- Demand; Uten NP-Gruppen ------------------ #
 
-    data_demand_UtenNP = data_uNP[data['price_area'] == price_area].copy()
+    data_demand_UtenNP = data_uNP[data_uNP['price_area'] == price_area].copy()
     data_demand_UtenNP['Date'] = pd.to_datetime(data_demand_UtenNP['Date'])
     data_demand_UtenNP['Hour'] = data_demand_UtenNP['Hour'].astype(int)
 
@@ -59,26 +75,25 @@ def DifferenceinDifference(data, price_area, Temp):
 
     data_demand_UtenNP_filtered_after = data_demand_UtenNP[(data_demand_NP['Date'] >= start_date_after) &
                                         (data_demand_UtenNP['Date'] <= end_date_after)].copy()
-    # print(data_demand_filtered1)
-    # print(data_demand_filtered2)
+    #print(data_demand_filtered1)
+    #print(data_demand_filtered2)
 
-    for index, rad in data_demand_NP_filtered_before.iterrows():
-        data_demand_UtenNP_filtered_before.loc[index, 'kWh/Metering_point'] = rad['quantity_kwh'] / rad['metering_point_count']
 
-    for index, rad in data_demand_NP_filtered_after.iterrows():
-        data_demand_UtenNP_filtered_after.loc[index, 'kWh/Metering_point'] = rad['quantity_kwh'] / rad['metering_point_count']
+    data_demand_UtenNP_filtered_before['kWh/Metering_point'] = data_demand_UtenNP_filtered_before['consumption_kwh'] / data_demand_UtenNP_filtered_before['metering_point_count']
+    #print(data_demand_UtenNP_filtered_before.head(3))
 
-    # print(data_demand_filtered2.head(3))
+    data_demand_UtenNP_filtered_after['kWh/Metering_point'] = data_demand_UtenNP_filtered_after['consumption_kwh'] / data_demand_UtenNP_filtered_after['metering_point_count']
+    #print(data_demand_UtenNP_filtered_after.head(3))
 
-    total_demand_hour_UtenNP_before = data_demand_UtenNP_filtered_before.groupby(['Date', 'Hour'])['kWh/Metering_point'].sum().reset_index()
-    total_demand_hour_UtenNP_after = data_demand_UtenNP_filtered_after.groupby(['Date', 'Hour'])['kWh/Metering_point'].sum().reset_index()
+    total_demand_hour_UtenNP_before = data_demand_UtenNP_filtered_before.groupby(['Date', 'Hour', 'group_definition'])['kWh/Metering_point'].sum().reset_index()
+    total_demand_hour_UtenNP_after = data_demand_UtenNP_filtered_after.groupby(['Date', 'Hour', 'group_definition'])['kWh/Metering_point'].sum().reset_index()
 
-    # print(total_hour1)
-    # print(total_hour2)
+    #print(total_demand_hour_UtenNP_before)
+    #print(total_demand_hour_UtenNP_after)
 
     # -------------- Temperatur --------------- #
 
-    Temp['Date'] = pd.to_datetime(Temp['Date'])
+    '''Temp['Date'] = pd.to_datetime(Temp['Date'])
     Temp['Hour'] = Temp['Hour'].astype(float)
 
     Temp['Temp24'] = Temp['Lufttemperatur'].rolling(window=24, min_periods=1).mean()
@@ -90,28 +105,46 @@ def DifferenceinDifference(data, price_area, Temp):
     Temp_filtered_after = Temp[(Temp['Date'] >= start_date_after) &
                           (Temp['Date'] <= end_date_after)].copy()
 
-    # total_temp_hour1 = Temp_filtered1.groupby(['Date', 'Hour'])['Lufttemperatur'].sum().reset_index()
-    # total_temp_hour2 = Temp_filtered2.groupby(['Date', 'Hour'])['Lufttemperatur'].sum().reset_index()
+    #total_temp_hour1 = Temp_filtered1.groupby(['Date', 'Hour'])['Lufttemperatur'].sum().reset_index()
+    #total_temp_hour2 = Temp_filtered2.groupby(['Date', 'Hour'])['Lufttemperatur'].sum().reset_index()
 
-    # print(total_temp_hour2.head(3))
+    # print(total_temp_hour2.head(3))'''
 
-    # -------- Merge data; NP-Gruppen ----------- #
-    merged_NP = pd.merge(total_demand_hour_NP_before, total_demand_hour_NP_after, Temp_filtered_before, on=['Date', 'Hour'])
+    # -------- Merge data; NP-Gruppen m/Temp ----------- #
+    '''merged_NP = pd.merge(total_demand_hour_NP_before, total_demand_hour_NP_after, Temp_filtered_before, on=['Date', 'Hour'])
     filtered_NP = merged_NP[(merged_NP['kWh/Metering_point'] > 0) & (merged_NP['Lufttemperatur'].notnull())].copy()
 
     df_NP = pd.DataFrame(filtered_NP)
 
     # pd.set_option('display.max_columns', None)
-    # print(df1.head(3))
+    # print(df1.head(3))'''
 
-    # ------------ Merge data; Uten NP-gruppen ----------- #
-    merged_UtenNP = pd.merge(total_demand_hour_UtenNP_before, total_demand_hour_UtenNP_after, Temp_filtered_after, on=['Date', 'Hour'])
+    # -------- Merge data; NP-Gruppen u/Temp ----------- #
+    df_NP_before = pd.DataFrame(total_demand_hour_NP_before)
+    df_NP_after = pd.DataFrame(total_demand_hour_NP_after)
+
+    df_NP = pd.concat([df_NP_before,df_NP_after], ignore_index=True)
+
+    pd.set_option('display.max_columns', None)
+    #print(df_NP)
+
+    # ------------ Merge data; Uten NP-gruppen m/temp ----------- #
+    '''merged_UtenNP = pd.merge(total_demand_hour_UtenNP_before, total_demand_hour_UtenNP_after, Temp_filtered_after, on=['Date', 'Hour'])
     filtered_UtenNP = merged_UtenNP[(merged_UtenNP['kWh/Metering_point'] > 0) & (merged_UtenNP['Lufttemperatur'].notnull())].copy()
 
     df_UtenNP = pd.DataFrame(filtered_UtenNP)
 
     # pd.set_option('display.max_columns', None)
-    # print(df2.head(3))
+    # print(df2.head(3))'''
+
+    # ------------ Merge data; Uten NP-gruppen u/Temp----------- #
+    df_UtenNP_before = pd.DataFrame(total_demand_hour_UtenNP_before)
+    df_UtenNP_after = pd.DataFrame(total_demand_hour_UtenNP_after)
+
+    df_UtenNP = pd.concat([df_UtenNP_before, df_UtenNP_after], ignore_index=True)
+
+    pd.set_option('display.max_columns', None)
+    #print(df_UtenNP)
 
     # ----------------- Beregninger; NP-Gruppen ------------------- #
     df_NP['Date'] = pd.to_datetime(df_NP['Date'])
@@ -136,8 +169,8 @@ def DifferenceinDifference(data, price_area, Temp):
                                   ordered=True)
 
     # --------------- Regresjonsanalyse ------------- #
-    df_NP['Group'] = 'Before_ref'  # 2024-periode
-    df_UtenNP['Group'] = 'After_ref'  # 2025-periode
+    #df_NP['Group'] = 'Norgespris'  # 2024-periode
+    #df_UtenNP['Group'] = 'Uten Norgespris'  # 2025-periode
 
     df_NP['Norgespris'] = 'Med_NP'  #
     df_UtenNP['Norgespris'] = 'Uten_NP'  #
@@ -145,14 +178,17 @@ def DifferenceinDifference(data, price_area, Temp):
 
     df = pd.concat([df_NP, df_UtenNP], ignore_index=True)
 
-    pd.set_option('display.max_columns', None)
-    print(df)
+    df['Group'] = np.where(df['Date'] < '2025-10-01', 'Before_ref', 'After_ref')
+
+    #pd.set_option('display.max_columns', None)
+    #pd.set_option('display.max_rows', None)
+    #print(df[['Date', 'Norgespris','Group']])
+    #print(df)
 
     formula = (
-        'Q("kWh/Metering_point") ~ '
-        'C(Group, Treatment(reference="Before_ref")) + C(Norgespris, Treatment(reference="Uten_NP"))'
-
-
+        'np.log(Q("kWh/Metering_point")) ~ '
+        'C(Group, Treatment(reference="Before_ref")) '
+        '* C(Norgespris, Treatment(reference="Uten_NP"))'
     )
 
     y, X = patsy.dmatrices(
@@ -166,7 +202,7 @@ def DifferenceinDifference(data, price_area, Temp):
     print(model.summary())
 
 
-DifferenceinDifference(data_mNP, 'NO1', Temp_Oslo)  # Ved NO1 bruk Temp_Oslo, og ved NO5 bruk Temp_Bergen
+DifferenceinDifference(data_mNP, data_uNP, 'NO1', Temp_Oslo)  # Ved NO1 bruk Temp_Oslo, og ved NO5 bruk Temp_Bergen
 
 
 
