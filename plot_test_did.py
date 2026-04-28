316e62ce0f312f1bde381e40932859d1f1404467
KristineMoen <kjmoen@stud.ntnu.no>
2026-04-28 14:17:33 +0200

    Endringer

--- plot_test_did.py
+++ plot_test_did.py
@@ -3,21 +3,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from networkx.algorithms.bipartite.basic import color
 
-# --------------------------------------------------
-# 1. Månedlig tidsakse: jan 2024 – sep 2025 (21 mnd)
-# --------------------------------------------------
+
 dates = pd.date_range(
     start="2024-10-01",
     end="2026-01-01",
     freq="MS"
 )
 
-# --------------------------------------------------
-# 2. Fyll inn DINE DiD-verdier her
-#    (21 tall i hver liste – én per måned)
-# --------------------------------------------------
-
-# ===== PRISSONE 1 =====
+# -------- NO1 -------- #
 did_z1 = [-1,-0.04,-0.55,0.15,0.45,0.71,-0.65,2.18,0.9,-0.04,1.33,1.5,2.74,2.47,3.24,4.63
     # okt24, nov24, des24, ..., sep25 + okt25, nov25, des25, jan26
 ]
@@ -26,7 +19,7 @@ did_z1_temp = [-0.16,0.75,0.96,1.5,0.43,1.09,0.48,0.33,0.8,1.59,1.37,1.56,2.66,2
     # okt24, nov24, des24, ..., sep25 + okt25, nov25, des25, jan26
 ]
 
-# ===== PRISSONE 2 =====
+# -------- NO2 -------- #
 did_z2 = [-0.05,0.44,0.24,0.64,0.42,0.87,0.86,0.74,0.92,1.28,1.8,1.89, 3.3,3.76,4.79,4.97
     # okt24, nov24, des24, ..., sep25 + okt25, nov25, des25, jan26
 ]
@@ -35,7 +28,7 @@ did_z2_temp = [-0.52,0.29,0.41,1.75,0.32,0.82,0.56,1.56,0.86,1.05,1.83,1.68,3.34
     # okt24, nov24, des24, ..., sep25 + okt25, nov25, des25, jan26
 ]
 
-# ===== PRISSONE 3 =====
+# -------- NO5 -------- #
 did_z5 = [-0.24,0.62,0.28,0.56,0.03,1.31,0.08,1.19,0.57,0.92,1.09,1.37, 2.06,2.32,2.76,3.4
     # okt24, nov24, des24, ..., sep25 + okt25, nov25, des25, jan26
 ]
@@ -43,30 +36,7 @@ did_z5 = [-0.24,0.62,0.28,0.56,0.03,1.31,0.08,1.19,0.57,0.92,1.09,1.37, 2.06,2.3
 did_z5_temp = [-0.05,0.74,0.81,0.8,0.04,1.34,0.19,0.48,0.49,1.52,1.16,1.3,2.01,2.32,2.76,2.72
     # okt24, nov24, des24, ..., sep25 + okt25, nov25, des25, jan26
 ]
-
-# --------------------------------------------------
-# 3. Plot: alle 6 serier i samme figur
-# --------------------------------------------------
-'''plt.figure(figsize=(14, 7))
-
-plt.plot(dates, did_z1,"--", label="NO1", linewidth=2, color= 'blue')
-plt.plot(dates, did_z1_temp, label="NO1 w/Temp", color = 'blue')
-
-plt.plot(dates, did_z2, "--", label="NO2", linewidth=2, color = 'red')
-plt.plot(dates, did_z2_temp,  label="NO2 w/Temp", color = 'red')
-
-plt.plot(dates, did_z5, "--", label="NO5", linewidth=2, color = 'green')
-plt.plot(dates, did_z5_temp,  label="NO5 w/Temp", color = 'green')
-
-# --------------------------------------------------
-# 4. Aksen og utseende
-# --------------------------------------------------
-plt.axhline(0, color="black", linewidth=1)
-plt.xlabel("Month")
-plt.ylabel("Difference in Difference ")
-plt.title("Difference in Differences – Monthly estimates")
-plt.legend(ncol=2)
-plt.grid(True)'''
+ # ----------- PLOT ----------- #
 
 fig, axes = plt.subplots(
     nrows=3,
@@ -75,7 +45,7 @@ fig, axes = plt.subplots(
     sharex=True
 )
 
-# Plot prissone 1
+# -------- NO1 --------
 #axes[0].plot(dates, did_z1, label="NO1", color="blue", marker = 'o', linestyle = 'None')
 axes[0].plot(dates, did_z1_temp, label="NO1 w/Temp",color="green", marker = 'o', linestyle = 'None')
 axes[0].set_title("DiD – NO1")
@@ -84,7 +54,7 @@ axes[0].axvline(x = pd.Timestamp('2025-10-01'), color = 'red', linestyle = '--',
 axes[0].axvspan(pd.Timestamp("2025-10-01"), pd.Timestamp("2026-01-31"), color = 'lightcoral', alpha = 0.15, zorder=0)
 axes[0].grid(True)
 
-# Plot prissone 2
+# -------- NO2 --------
 #axes[1].plot(dates, did_z2, label="NO2", color="blue", marker = 'o', linestyle = 'None')
 axes[1].plot(dates, did_z2_temp, label="NO2 w/Temp",color="green", marker = 'o', linestyle = 'None')
 axes[1].set_title("DiD – NO2")
@@ -93,7 +63,7 @@ axes[1].axvline(x = pd.Timestamp('2025-10-01'), color = 'red', linestyle = '--',
 axes[1].axvspan(pd.Timestamp("2025-10-01"), pd.Timestamp("2026-01-31"), color = 'lightcoral', alpha = 0.15, zorder=0)
 axes[1].grid(True)
 
-# Plot prissone 3
+# -------- NO5 --------
 #axes[2].plot(dates, did_z5, label="NO5", color="blue", marker = 'o', linestyle = 'None')
 axes[2].plot(dates, did_z5_temp, label="NO5 w/Temp", color="green", marker = 'o', linestyle = 'None')
 axes[2].set_title("DiD – NO5")
@@ -103,12 +73,10 @@ axes[2].axvspan(pd.Timestamp("2025-10-01"), pd.Timestamp("2026-01-31"), color = 
 axes[2].set_xlabel("Date")
 axes[2].grid(True)
 
-# --------------------------------------------------
-# 4. Vis alt i ett vindu
-# --------------------------------------------------
-from matplotlib.lines import Line2D
 
-'''temp_legend_elements = [
+'''from matplotlib.lines import Line2D
+
+temp_legend_elements = [
     Line2D([0], [0], marker = 'o',
            color='blue',
            linestyle='None',
